45cd514d5e46e09f9eb4358df14af14cebd5decc
Brendan Ballard <brendan.ballard96@yahoo.com>
2023-10-11 20:23:14 -0500

    add deleteTask function and add to ToDo component

--- moz-todo-react/src/App.js
+++ moz-todo-react/src/App.js
@@ -11,7 +11,17 @@ function App(props) {
     setTasks([...tasks, newTask])
   }
   function toggleTaskCompleted(id) {
-    console.log(tasks[0])
+    const updatedTasks = tasks.map((task) => {
+      if (id === task.id) {
+        return { ...task, completed:
+        !task.completed }
+      }
+      return task;
+    })
+    setTasks(updatedTasks)
+  }
+  function deleteTask(id) {
+    console.log(id)
   }
   const taskList = tasks.map((task) => (
     <Todo
@@ -20,6 +30,7 @@ function App(props) {
     completed={task.completed}
     key={task.id}
     toggleTaskCompleted={toggleTaskCompleted}
+    deleteTask={deleteTask}
     />
     ));
     const tasksNoun = taskList.length !== 1 ? 'tasks' : 'task';
