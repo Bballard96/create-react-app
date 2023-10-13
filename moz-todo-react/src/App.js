3e23913aedd0967198a099787344a053d56e7975
Brendan Ballard <brendan.ballard96@yahoo.com>
2023-10-13 09:52:30 -0500

    add editTask

--- moz-todo-react/src/App.js
+++ moz-todo-react/src/App.js
@@ -24,6 +24,16 @@ function App(props) {
     const remainingTasks = tasks.filter((task) => id !== task.id)
     setTasks(remainingTasks);
   }
+  function editTask(id, newName) {
+    const editedTaskList = tasks.map((task) => {
+      if (id === task.id) {
+        return { ...task, name: newName };
+      }
+      return task
+    })
+    setTasks(editedTaskList)
+  }
+
   // comment to push
   const taskList = tasks.map((task) => (
     <Todo
@@ -33,6 +43,7 @@ function App(props) {
     key={task.id}
     toggleTaskCompleted={toggleTaskCompleted}
     deleteTask={deleteTask}
+    editTask={editTask}
     />
     ));
     const tasksNoun = taskList.length !== 1 ? 'tasks' : 'task';
